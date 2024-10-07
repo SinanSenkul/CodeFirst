c49ba8fc244230e4fea321dd5899085e07aac5e5
sinan <sinan@SINAN-SENKUL>
2024-10-07 23:18:10 +0300

    Add project files.

---
+++ CodeFirst/Contexts/PatikaFirstDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CodeFirst.Contexts
+{
+    public class PatikaFirstDBContext : DbContext
+    {
+        public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movies");
+            modelBuilder.Entity<Game>().ToTable("Games");
+        }
+    }
+}
